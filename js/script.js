229943fb7e2fbaa45e99bd40ab2940e9860dbd88
Ivan Zadorozhniy <vakikrin@gmail.com>
2018-03-01 15:41:37 +0200

    added block for search

--- js/script.js
+++ js/script.js
@@ -1,20 +1,40 @@
-$("document").ready(function () {
-//    alert("Dsa");
-    var userQuery = "Kiev";
+$("document").ready(function() {
+    //    alert("Dsa");
+    $("form").submit(function(event){
+        console.log( event );
+
+         var query = $("[name=q]").val();
+         console.log(query);
+        getArticles(query);
+        event.preventDefault();
+    });
+});
+function getArticles(usersQuery){
+     var userQuery = usersQuery;
     $.getJSON('https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrsearch=' + userQuery + '&callback=?') //used API from Forismatic - https://forismatic.com/ru/api/
         .done(update) //If the request succeeds, then the function is called - update
-        .fail(errors);// else the function is called - errors
+        .fail(errors); // else the function is called - errors
 
-
-});
-
+}
 function update(response) {
-  //  https://en.wikipedia.org/w/api.php?action=query&prop=info&pageids=18630637&inprop=url
-    console.log(JSON.stringify(response.query.pages));
+  //  console.log(JSON.stringify(response.query.pages));
     var obj = response.query.pages;
+    $("#content").html(''); //Clear content box;
     for (key in obj) {
-        console.log(obj[key]);
+        $.getJSON('https://en.wikipedia.org/w/api.php?action=query&prop=info&pageids=' + obj[key].pageid + '&inprop=url&format=json&callback=?')
+            .done(editUpdate)
+            .fail(errors);
     }
+}
+
+function editUpdate(response) {
+    console.log(response);
+    var objectOfPage = response.query.pages;
+    for (key in objectOfPage) {
+        $('#content').append("<a href = '" + objectOfPage[key].fullurl + "'>"+ "<div class='block-response'>" + objectOfPage[key].title +
+            "</div></a>");
+    }
+
 }
 
 function errors() {
